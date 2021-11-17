7b84fb397869e85468d7e282f3336b7560b337ed
unknown <jpriyanshjain>
2021-11-17 11:14:31 +0530

    after addin hook

--- src/container/ColorBody/ColorBody.jsx
+++ src/container/ColorBody/ColorBody.jsx
@@ -1,22 +1,24 @@
+import { useCallback } from "react";
 import IndividualColor from "../../Component/IndividualColor/IndividualColor";
 import "./ColorBody.css";
 
-//function for filling the values in array by iterating first kepping red color value const and
-// increasing green and blue value by 8 and once the value is
-//grater than 256 restart the loop by increasing the value of red color
-//and breaking the loop once the value of red is over 256
-const fillColors = (colors) => {
-  for (let redItr = 8; redItr <= 256; redItr += 8) {
-    for (let greenItr = 8; greenItr <= 256; greenItr += 8) {
-      for (let blueItr = 8; blueItr <= 256; blueItr += 8) {
-        colors.push([redItr, greenItr, blueItr]);
-      }
-    }
-  }
-};
-
 function ColorBody() {
   const colorsArray = [];
+
+  const fillColors = useCallback((colors) => {
+    //function for filling the values in array by iterating first kepping red color value const and
+    // increasing green and blue value by 8 and once the value is
+    //grater than 256 restart the loop by increasing the value of red color
+    //and breaking the loop once the value of red is over 256
+    for (let redItr = 8; redItr <= 256; redItr += 8) {
+      for (let greenItr = 8; greenItr <= 256; greenItr += 8) {
+        for (let blueItr = 8; blueItr <= 256; blueItr += 8) {
+          colors.push([redItr, greenItr, blueItr]);
+        }
+      }
+    }
+  }, []);
+
   //filling the RGB value in colors array
   fillColors(colorsArray);
 
